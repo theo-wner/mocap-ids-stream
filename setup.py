d7129293d48c1267f3ef2bbe50bf401decd22d64
theo-wner <theodor.kapler@student.kit.edu>
2025-07-10 19:20:47 +0200

    changed setup and requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mocap_ids_stream',
-    version='0.1.0',
+    version='0.0.0',
     packages=find_packages(include=['streams', 'streams.*']),
     install_requires=[
         'numpy',
