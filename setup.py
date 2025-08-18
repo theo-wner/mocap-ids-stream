072bfc6f4c8ddadaecdae982be52a86571a79f14
theo-wner <theodor.kapler@student.kit.edu>
2025-08-18 17:08:13 +0200

    added __init__.pys

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mocap_ids_stream',
     version='0.0.0',
-    packages=find_packages(include=['streams', 'streams.*']),
+    packages=find_packages(include=['streams', 'streams.*', 'calibration', 'calibration.*', 'capture', 'capture.*']),
     install_requires=[
         'numpy',
         'opencv-python==4.11.0.86',
